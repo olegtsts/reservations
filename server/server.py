529adf032d38b07eb4001ea4e0d92714340391e6
Oleg Tsarkov <olegtsts@gmail.com>
2019-02-03 02:58:08 +0300

    Refine some field names

--- server/server.py
+++ server/server.py
@@ -23,10 +23,10 @@ def send_file(path):
 def get_all_reservations():
     r = Redis(host='localhost', port=6379, db=0)
 
-    org = schema.Organisation
-    org.ParseFromString(r.get(organisation_id))
+    org = schema_pb2.Organisation
+    org.ParseFromString(r.get(request.form['organisation_id']))
 
-    org.reservation_secret.Clear();
+    org.reservation_secret.Clear()
 
     return jsonify({
         'data': org.SerializeToString()
@@ -35,18 +35,22 @@ def get_all_reservations():
 @app.route('/add_reservation', methods=['POST'])
 def add_reservation():
     r = Redis(host='localhost', port=6379, db=0)
-    org = schema.Organisation
+    org = schema_pb2.Organisation
+
+    reservation_request = schema_pb2.ReservationRequest.ParseFromString(request.form['data'])
+
     org.ParseFromString(r.get(reservation_request.organisation_id))
 
-    response = schema.ReservationResponce
+    response = schema_pb2.ReservationResponse()
+
 
     for reservation in org.reservation:
         if reservation.reservlet_id == reservation_request.reservlet_id:
             first_sign = reservation_request.time_from - reservation.time_to
             second_sign = reservation_request.time_to - reservation.time_from
             if (first_sign * second_sign < 0):
-                response.status = schema_pb2.ResponceStatus.FAILED
-                response.debug_message = "Given time is already reserved."
+                response.status = schema_pb2.ResponseStatus.ALREADY_RESERVED
+                response.error_reason = "Given time is already reserved."
                 return response
 
     new_reservation = org.reservation.add()
@@ -59,11 +63,11 @@ def add_reservation():
     new_secret_code = org.reservation_secret.add()
 
     new_secret_code.secret_code = hashlib.blake2b(digest_size=5).update(str(datetime.datetime.now().time()) + reservation_request.reservlet_id)
-    new_secret_code.reservlet_id = reservation_request.reservlet_id
+    new_secret_code.reservation_id = new_reservation.id
 
-    r.set(eservation_request.organisation_id, org.SerializeToString())
+    r.set(reservation_request.organisation_id, org.SerializeToString())
 
-    response.status = schema_pb2.ResponceStatus.SUCCESS
+    response.status = schema_pb2.ResponseStatus.SUCCESS
     response.secret_code = new_secret_code.secret_code
 
     return jsonify({
