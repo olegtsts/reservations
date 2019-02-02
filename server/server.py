9110b25e8bfa2e4d4bcaa9b93553401d07a96977
Oleg Tsarkov <olegtsts@gmail.com>
2019-02-03 02:43:57 +0300

    Adding preparation script, deleting helpers lib, fixing spaces.

--- server/server.py
+++ server/server.py
@@ -20,18 +20,55 @@ def send_file(path):
     return send_from_directory('files', path)
 
 @app.route('/get_all_reservations', methods=['POST'])
-def respond_to_ajax():
+def get_all_reservations():
+    r = Redis(host='localhost', port=6379, db=0)
+
+    org = schema.Organisation
+    org.ParseFromString(r.get(organisation_id))
+
+    org.reservation_secret.Clear();
+
     return jsonify({
-        'data': get_all_reservations(request.form['restaurant_id'])
+        'data': org.SerializeToString()
     })
 
 @app.route('/add_reservation', methods=['POST'])
-def respond_to_ajax():
+def add_reservation():
+    r = Redis(host='localhost', port=6379, db=0)
+    org = schema.Organisation
+    org.ParseFromString(r.get(reservation_request.organisation_id))
+
+    response = schema.ReservationResponce
+
+    for reservation in org.reservation:
+        if reservation.reservlet_id == reservation_request.reservlet_id:
+            first_sign = reservation_request.time_from - reservation.time_to
+            second_sign = reservation_request.time_to - reservation.time_from
+            if (first_sign * second_sign < 0):
+                response.status = schema_pb2.ResponceStatus.FAILED
+                response.debug_message = "Given time is already reserved."
+                return response
+
+    new_reservation = org.reservation.add()
+
+    new_reservation.id = hashlib.blake2b(digest_size=10).update(str(reservation_request.SerializeToString()))
+    new_reservation.time_from = reservation_request.time_from
+    new_reservation.time_to = reservation_request.time_to
+    new_reservation.reservlet_id = reservation_request.reservlet_id
+
+    new_secret_code = org.reservation_secret.add()
+
+    new_secret_code.secret_code = hashlib.blake2b(digest_size=5).update(str(datetime.datetime.now().time()) + reservation_request.reservlet_id)
+    new_secret_code.reservlet_id = reservation_request.reservlet_id
+
+    r.set(eservation_request.organisation_id, org.SerializeToString())
+
+    response.status = schema_pb2.ResponceStatus.SUCCESS
+    response.secret_code = new_secret_code.secret_code
+
     return jsonify({
-        'responce': add_reservation(request.form['request_proto'])
+        'response': response.SerializeToString()
     })
-
-
 
 def run_server():
     app.run(
@@ -43,3 +80,4 @@ def run_server():
 
 if __name__ == "__main__":
     run_server()
+
