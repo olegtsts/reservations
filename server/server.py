cbd2537ec718de9cc72d5296e536967ebb63c078
Oleg Tsarkov <olegtsts@gmail.com>
2019-02-03 03:01:34 +0300

    Fix syntax

--- server/server.py
+++ server/server.py
@@ -23,7 +23,7 @@ def send_file(path):
 def get_all_reservations():
     r = Redis(host='localhost', port=6379, db=0)
 
-    org = schema_pb2.Organisation
+    org = schema_pb2.Organisation()
     org.ParseFromString(r.get(request.form['organisation_id']))
 
     org.reservation_secret.Clear()
@@ -35,21 +35,19 @@ def get_all_reservations():
 @app.route('/add_reservation', methods=['POST'])
 def add_reservation():
     r = Redis(host='localhost', port=6379, db=0)
-    org = schema_pb2.Organisation
-
     reservation_request = schema_pb2.ReservationRequest.ParseFromString(request.form['data'])
 
+    org = schema_pb2.Organisation()
     org.ParseFromString(r.get(reservation_request.organisation_id))
 
     response = schema_pb2.ReservationResponse()
-
 
     for reservation in org.reservation:
         if reservation.reservlet_id == reservation_request.reservlet_id:
             first_sign = reservation_request.time_from - reservation.time_to
             second_sign = reservation_request.time_to - reservation.time_from
             if (first_sign * second_sign < 0):
-                response.status = schema_pb2.ResponseStatus.ALREADY_RESERVED
+                response.status = schema_pb2.ReservationResponse.ALREADY_RESERVED
                 response.error_reason = "Given time is already reserved."
                 return response
 
@@ -67,7 +65,7 @@ def add_reservation():
 
     r.set(reservation_request.organisation_id, org.SerializeToString())
 
-    response.status = schema_pb2.ResponseStatus.SUCCESS
+    response.status = schema_pb2.ReservationResponse.SUCCESS
     response.secret_code = new_secret_code.secret_code
 
     return jsonify({
