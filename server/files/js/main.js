3b6bd5bd7ed8b38d20210eeb28d6614de1ae921a
Oleg Tsarkov <olegtsts@gmail.com>
2019-02-10 00:19:39 +0300

    Refactoring menu to be class.

--- server/files/js/main.js
+++ server/files/js/main.js
@@ -1,19 +1,6 @@
-function do_something() {
-    $.ajax({
-        url: '/ajax',
-        method: 'POST',
-        data: {
-            smth: 1
-        },
-        success: function(result) {
-            alert(result.response);
-        }
-    });
-}
-
 $(function () {
     new State({
-        ...get_menu_states({
+        ...new Menu({
             'id': 'test',
             'container': function () {
                 return $('#container');
@@ -31,7 +18,7 @@ $(function () {
                     'name': 'Second',
                 },
             ],
-        }),
+        }).get_menu_states(),
         'test_menu_result': new Combine([
             new Executer(function (context) {
                 alert(context.menu_result);
--- server/files/js/menu.js
+++ server/files/js/menu.js
@@ -1,69 +1,77 @@
-function get_menu_buttons_html(params) {
+function Menu(params) {
+    this.params = params;
+}
+
+Menu.prototype.get_menu_buttons_html = function() {
     var buttons_html = '<div class="col-6 offset-3 col-lg-2 offset-lg-5" style="margin-top:15%">';
-    for (var i = 0; i < params.options.length; ++i) {
-        var option = params.options[i];
-        buttons_html += '<div><button type="button" class="btn btn-primary btn-block btn-lg" name="' + option.id + '" id="' + params.id + '_' + option.id + '">' + option.name + '</button> </div><br>';
+    for (var i = 0; i < this.params.options.length; ++i) {
+        var option = this.params.options[i];
+        buttons_html += '<div><button type="button" class="btn btn-primary btn-block btn-lg" name="' +
+            option.id + '" id="' + this.params.id + '_' + option.id + '">' + option.name + '</button> </div><br>';
     }
     buttons_html += '</div>'
     return buttons_html;
 }
 
-function get_menu_builder(params) {
-    return  new Combine([
+Menu.prototype.get_menu_builder = function() {
+    var _this = this;
+    return new Combine([
         new Builder({
-            'container': params.container,
+            'container': this.params.container,
             'func': function (context, container) {
                 container.append(`
                     <div class="text-center">
-                        ` + get_menu_buttons_html(params) + `
+                        ` + _this.get_menu_buttons_html() + `
                     </div>
                 `);
             },
         }),
         new GoTo({
-            'new_state': params.id + '::listen',
+            'new_state': this.params.id + '::listen',
             'type': 'substate',
         })
     ]);
 }
 
-function get_button_target(params, option) {
+Menu.prototype.get_button_target = function(option) {
+    var _this = this;
     return function () {
-        return $('#' + params.id + '_' + option.id);
+        return $('#' + _this.params.id + '_' + option.id);
     }
 }
 
-function get_menu_binders(params) {
+Menu.prototype.get_menu_binders = function() {
     var binders = [];
-    for (var i = 0; i < params.options.length; ++i) {
-        var option = params.options[i];
+    for (var i = 0; i < this.params.options.length; ++i) {
+        var option = this.params.options[i];
         binders.push(new Binder({
-            'target': get_button_target(params, option),
+            'target': this.get_button_target(option),
             'action': 'click',
             'type': 'next',
             'write_to': 'pressed_button',
-            'new_state': params.id + '::write_result',
+            'new_state': this.params.id + '::write_result',
         }));
     }
     return new Combine(binders);
 }
 
-function get_menu_result_writer(params) {
+Menu.prototype.get_menu_result_writer = function() {
+    var _this = this;
     return new Combine([
         new Executer(function (context) {
-            context.parent[params.write_to] = context.pressed_button.attr('name');
+            context.parent[_this.params.write_to] = context.pressed_button.attr('name');
         }),
         new GoTo({
             'type': 'exit_state',
-            'new_state': params.final_state,
+            'new_state': this.params.final_state,
         }),
     ]);
 }
 
-function get_menu_states(params) {
+Menu.prototype.get_menu_states = function() {
     var menu_states = {};
-    menu_states[params.initial_state] = get_menu_builder(params);
-    menu_states[params.id + '::listen'] = get_menu_binders(params);
-    menu_states[params.id + '::write_result'] = get_menu_result_writer(params);
+    menu_states[this.params.initial_state] = this.get_menu_builder();
+    menu_states[this.params.id + '::listen'] = this.get_menu_binders();
+    menu_states[this.params.id + '::write_result'] = this.get_menu_result_writer();
     return menu_states;
 }
