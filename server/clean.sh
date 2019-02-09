4f5bd2638693313a7083c62566553b7f26a76960
Oleg Tsarkov <olegtsts@gmail.com>
2019-02-10 02:10:07 +0300

    Adding gitignore, changing proto js files path

--- server/clean.sh
+++ server/clean.sh
@@ -1,7 +1,4 @@
-rm files/js/organization.js
-rm files/js/reservations.js
-rm files/js/reservationrequest.js
-rm files/js/reservationresponse.js
-rm schema_pb2.py
+rm -rf files/js/proto
+rm -f schema_pb2.py
 rm -rf files/closure-library
 rm -rf files/protobuf
--- server/prepare.sh
+++ server/prepare.sh
@@ -1,5 +1,5 @@
 protoc -I=. --python_out=. schema.proto
 python setup-organization.py
-protoc --js_out=files/js  schema.proto
+protoc --js_out=files/js/proto schema.proto
 git clone https://github.com/google/closure-library files/closure-library
 git clone git@github.com:protocolbuffers/protobuf.git files/protobuf
