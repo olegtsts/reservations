306e861746c2901f8dfa9a43a32a78508cdd5aef
Oleg Tsarkov <olegtsts@gmail.com>
2019-02-10 01:08:58 +0300

    Generating javascript proto libraries. Adding clean script.

---
+++ server/clean.sh
@@ -0,0 +1,5 @@
+rm files/js/organisation.js
+rm files/js/reservations.js
+rm files/js/reservationrequest.js
+rm files/js/reservationresponse.js
+rm schema_pb2.py
--- server/prepare.sh
+++ server/prepare.sh
@@ -1,2 +1,3 @@
 protoc -I=. --python_out=. schema.proto
 python setup-organization.py
+protoc --js_out=files/js  schema.proto
